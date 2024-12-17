08decfb01f06774659bd45872396fc05f6ca98b2
Roma <roman.iuklenchuk@nure.ua>
2024-12-17 11:42:50 +0200

    main merge

--- Task2/WarehouseRent/backend/app/database/__init__.py
+++
@@ -1,3 +0,0 @@
-from .conn import engine, url, get_db
-from .base_model import Base
-from .models import User, Warehouse, Rental, Payment, Lock, Message, UserRole, PremiumService
--- Task2/WarehouseRent/backend/app/database/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Warehouse import Warehouse
-from .Rental import Rental, RentalStatus
-from .Payment import Payment, PaymentStatus
-from .User import User, UserRole
-from .Lock import Lock
-from .Message import Message
-from .PremiumService import PremiumService
